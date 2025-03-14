648bc0bbf154d2266ea77b4bde520d29a7a3fe52
Clarissa Jaime <clarissanjaime@gmail.com>
2025-03-14 16:17:36 -0500

    Move util helpers to root

--- helpers/utils.js
+++
@@ -1,3 +0,0 @@
-export function mockDelay(ms) {
-    return new Promise((resolve) => setTimeout(resolve, ms));
-  }
---
+++ utils.js
@@ -0,0 +1,15 @@
+export function mockDelay(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+export const range = (start, end, step = 1) => {
+  let output = [];
+  if (typeof end === "undefined") {
+    end = start;
+    start = 0;
+  }
+  for (let i = start; i < end; i += step) {
+    output.push(i);
+  }
+  return output;
+};
