c94708828e01d7a0e8e2cf14749b7c2669eae122
Clarissa Jaime <clarissanjaime@gmail.com>
2025-04-11 11:36:52 -0500

    Hide animations on mobile

--- app/page.tsx
+++ app/page.tsx
@@ -31,8 +31,10 @@ const MAX_FEATURED_POSTS = 4;
 export default async function Home(): Promise<React.ReactElement> {
   return (
     <div className="flex min-h-screen flex-col">
-      <AnimatedDoodles />
-      <FloatingIcons />
+      <div className="hidden md:block">
+        <AnimatedDoodles />
+        <FloatingIcons />
+      </div>
       <main className="flex-1">
         <section className="w-full py-12 md:py-24 lg:py-32 bg-muted">
           <div className="container px-4 md:px-6">
